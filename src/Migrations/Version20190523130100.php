3595d3a76c2238fe55c9c946a9468e1fa4d8404d
felagund <zangeneh.saeed411@yahoo.com>
2019-05-24 19:53:29 +0430

    temp

--- src/Migrations/Version20190523130100.php
+++ src/Migrations/Version20190523130100.php
@@ -22,7 +22,7 @@ final class Version20190523130100 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sms (id INT AUTO_INCREMENT NOT NULL, message_body VARCHAR(511) NOT NULL, phone_number VARCHAR(15) NOT NULL, creation_date DATETIME NOT NULL, sent_state INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sms (id INT AUTO_INCREMENT NOT NULL, message_body VARCHAR(511) NOT NULL, api INT, phone_number VARCHAR(15) NOT NULL, creation_date DATETIME NOT NULL, sent_state INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
